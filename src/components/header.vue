37e475142fcd551feacbaad2dab2f9808742a334
nnuyz <hellonedesing@gmail.com>
2023-04-06 22:49:02 +0800

    add header component

---
+++ src/components/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="global-header-component w-100 h-auto fixed">
+    <div class="w-container flex a-center j-between">
+      <div class="logo c-default">vue-<span class="text-gradient">pokemon</span>-shop</div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'GlobalHeaderComponents'
+}
+</script>
