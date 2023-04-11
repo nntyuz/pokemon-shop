ee33a750017e7045dbfd064e2fe54a98662c8334
nnuyz <hellonedesing@gmail.com>
2023-04-11 22:17:24 +0800

    change app + add components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <global-header-component />
-  <section class="w-container content"></section>
+  <section class="w-container flex a-center j-between wrap gap-20 pt-100">
+    <pokemon-card />
+  </section>
+  <global-footer-component />
 </template>
 
 <script>
-import GlobalHeaderComponent from './components/header.vue'
+import GlobalHeaderComponent from './components/global/header.vue'
+import PokemonCard from './components/card.vue'
+import GlobalFooterComponent from './components/global/footer.vue'
 export default {
   components: {
-    GlobalHeaderComponent
+    GlobalHeaderComponent,
+    PokemonCard,
+    GlobalFooterComponent
   }
 }
 </script>
