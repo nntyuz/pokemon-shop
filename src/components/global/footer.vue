d59dcaefd9af89eb00f7462f0c098a9214bc7763
nnuyz <hellonedesing@gmail.com>
2023-04-11 22:16:30 +0800

    add footer component + create global folder

---
+++ src/components/global/footer.vue
@@ -0,0 +1,43 @@
+<template>
+  <footer class="global-footer-component mt-40">
+    <div class="w-container flex a-center j-center gap-20">
+      <a
+        v-for="(link, index) in links"
+        :key="index"
+        :href="link.href"
+        target="_blank"
+        class="links flex a-center gap-8 text-base font-bold text-dark"
+      >
+        <vIcon :name="link.icon" />
+        {{ link.name }}
+      </a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'GlobalFooterComponent',
+  data() {
+    return {
+      links: [
+        {
+          href: 'https://github.com/nnuyz',
+          icon: 'github',
+          name: 'GitHub'
+        },
+        {
+          href: 'https://t.me/nntyuz',
+          icon: 'telegram',
+          name: 'Telegram'
+        },
+        {
+          href: 'mailto:hellonedesign@gmail.com',
+          icon: 'mail',
+          name: 'Mail'
+        }
+      ]
+    }
+  }
+}
+</script>
--- src/components/global/header.vue
+++ src/components/global/header.vue
@@ -1,7 +1,9 @@
 <template>
   <header class="global-header-component w-100 h-auto fixed">
     <div class="w-container flex a-center j-between">
-      <div class="logo c-default">vue-<span class="text-gradient">pokemon</span>-shop</div>
+      <div class="logo text-large font-bold c-default">
+        vue-<span class="text-gradient">pokemon</span>-shop
+      </div>
     </div>
   </header>
 </template>
