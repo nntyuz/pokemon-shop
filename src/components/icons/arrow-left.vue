107bcbaade34ebabf6f0deda1141dfea06e84b89
nnuyz <hellonedesing@gmail.com>
2023-04-11 22:16:55 +0800

    add icons

---
+++ src/components/icons/arrow-left.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M16.0077 4.08001L9.48766 10.6C8.71766 11.37 8.71766 12.63 9.48766 13.4L16.0077 19.92"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-miterlimit="10"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
