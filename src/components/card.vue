7e95170dc1246d7723592c9527bb928f38d9a027
nnuyz <hellonedesing@gmail.com>
2023-04-11 22:16:04 +0800

    add card

---
+++ src/components/card.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-for="(pokemon, idx) in pokemons" :key="idx" class="card c-pointer font-bold">
+    <div class="id text-small">{{ `#${pokemon.id}` }}</div>
+    <img
+      :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`"
+      alt="PokemonId"
+      width="96"
+      height="96"
+    />
+    <div class="name text-base">{{ pokemon.name }}</div>
+    <vButton type="primary" size="medium" class="font-bold">Catch</vButton>
+  </div>
+</template>
+
+<script>
+import { useMainStore } from '../stores/store.js'
+import { mapState } from 'pinia'
+export default {
+  name: 'CardComponent',
+  computed: {
+    ...mapState(useMainStore, {
+      pokemons: (store) => store.pokemons
+    })
+  },
+  created() {
+    const store = useMainStore()
+    store.getAllPokemons()
+  }
+}
+</script>
