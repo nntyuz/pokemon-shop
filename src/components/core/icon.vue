133f9a4a9c2f6a2b8c599b70dc07fbca681b9cf7
nnuyz <hellonedesing@gmail.com>
2023-04-15 18:44:22 +0800

    add new icons in icon components

--- src/components/core/icon.vue
+++ src/components/core/icon.vue
@@ -8,13 +8,16 @@
 import github from '../icons/github.vue'
 import telegram from '../icons/telegram.vue'
 import mail from '../icons/mail.vue'
-
+import arrowLeft from '../icons/arrow-left.vue'
+import arrowRight from '../icons/arrow-right.vue'
 export default {
   name: 'vIcon',
   components: {
     github,
     telegram,
-    mail
+    mail,
+    arrowLeft,
+    arrowRight
   },
   props: {
     name: String
