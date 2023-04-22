b0cc569a25bdf1a20f88d78af430d1eb25887bfc
nnuyz <hellonedesing@gmail.com>
2023-04-22 20:45:09 +0800

    change home component and delete content

--- src/page/home.vue
+++ src/page/home.vue
@@ -1,13 +1,47 @@
 <template>
-  <content-component />
+  <section class="w-container flex a-center j-between wrap gap-20 pt-100">
+    <card :pokemons="pokemons" />
+    <pagination :pages="Math.ceil(count / pokemonsPerPage)" :page="page" @pagination="pagination" />
+  </section>
 </template>
 
 <script>
-import ContentComponent from '../components/content.vue'
+import { useMainStore } from '../stores/store.js'
+import { mapState } from 'pinia'
+import Card from '../components/card.vue'
+import Pagination from '../components/pagination.vue'
 export default {
   name: 'HomeComponent',
   components: {
-    ContentComponent
+    Card,
+    Pagination
+  },
+  data() {
+    return {
+      page: 1,
+      pokemonsPerPage: 20
+    }
+  },
+  computed: {
+    ...mapState(useMainStore, {
+      pokemons: (store) => store.pokemons,
+      count: (store) => store.count
+    })
+  },
+  created() {
+    this.page = Number(this.$route.query.page) || 1
+    this.getAllPokemons((this.page - 1) * this.pokemonsPerPage)
+  },
+  methods: {
+    getAllPokemons(offset) {
+      const store = useMainStore()
+      store.getAllPokemons(offset)
+    },
+    pagination(page) {
+      this.page = page
+      this.getAllPokemons((page - 1) * this.pokemonsPerPage)
+      this.$router.replace({ path: '/', query: { page } })
+    }
   }
 }
 </script>
