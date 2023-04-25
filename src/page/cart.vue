2930cc67ba339154ad0957005d6095b330e1f7c6
nnuyz <hellonedesing@gmail.com>
2023-04-25 20:35:54 +0800

    add cart-page

---
+++ src/page/cart.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="w-container flex a-center j-between wrap gap-20 pt-100">
+    <h1 v-if="cart.length === 0" class="text-large font-bold w-100 text-a-center">
+      Your captured Pokemons
+    </h1>
+    <card :pokemons="cart" />
+  </section>
+</template>
+
+<script>
+import { useMainStore } from '../stores/store.js'
+import { mapState } from 'pinia'
+import Card from '../components/card.vue'
+export default {
+  name: 'CartPage',
+  components: {
+    Card
+  },
+  computed: {
+    ...mapState(useMainStore, {
+      cart: (store) => store.cart
+    })
+  }
+}
+</script>
