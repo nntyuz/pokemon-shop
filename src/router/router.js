14475bbb281468c1443ded877ca657410b9e6c9d
nnuyz <hellonedesing@gmail.com>
2023-04-25 20:34:59 +0800

    add new route cart

--- src/router/router.js
+++ src/router/router.js
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../page/home.vue'
+import Cart from '../page/cart.vue'
 
-const routes = [{ name: 'home', path: '/', component: Home }]
+const routes = [
+  { name: 'home', path: '/', component: Home },
+  { name: 'cart', path: '/cart', component: Cart }
+]
 
 const router = createRouter({
   history: createWebHistory(),
