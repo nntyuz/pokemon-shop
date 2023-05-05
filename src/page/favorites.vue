bec07f9ffb7641520b66e99b00f43c683731b2af
nnuyz <hellonedesing@gmail.com>
2023-05-05 16:52:12 +0800

    add new favorite page

---
+++ src/page/favorites.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="w-container flex a-center j-between wrap gap-20 pt-100">
+    <h1 v-if="favorites.length === 0" class="text-large font-bold w-100 text-a-center">
+      Not a single favorite pokemon
+    </h1>
+    <card :pokemons="favorites" />
+  </section>
+</template>
+
+<script>
+import { useMainStore } from '../stores/store.js'
+import { mapState } from 'pinia'
+import Card from '../components/card.vue'
+export default {
+  name: 'Favorites',
+  components: {
+    Card
+  },
+  computed: {
+    ...mapState(useMainStore, {
+      favorites: (store) => store.favorites
+    })
+  }
+}
+</script>
--- src/router/router.js
+++ src/router/router.js
@@ -2,10 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../page/home.vue'
 import Cart from '../page/cart.vue'
+import Favorites from '../page/favorites.vue'
 
 const routes = [
   { name: 'home', path: '/', component: Home },
-  { name: 'cart', path: '/cart', component: Cart }
+  { name: 'cart', path: '/cart', component: Cart },
+  { name: 'favorites', path: '/favorites', component: Favorites }
 ]
 
 const router = createRouter({
